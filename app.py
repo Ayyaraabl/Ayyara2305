496f5aed3758a46701ccd8c8d64a607275d5c339
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-23 10:13:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide")
+  page_title="Kuliah Praktisi",
+  layout="centered")
 
 # Hirarki teks
 st.title("📊 Dashboard")
